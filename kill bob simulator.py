b425b057b07d751a27d524a5775cea5fea7a17e5
Xander Harris <xanderharris001@student.crowder.edu>
2024-06-06 16:31:26 -0500

    BOB... IS DEAD!

--- kill bob simulator.py
+++ kill bob simulator.py
@@ -28,22 +28,26 @@ player_points = 18
 player_defense = 0
 current_paction = 'whaat'
 
-while bob_health > 0:
+while bob_health > 0 and player_health > 0:
     current_paction = input('action?\nattack, action, check\n')
     player_defense = 0
     if current_paction == 'attack':
         #go through players action
         while current_paction == 'attack':
-            player_attack = int(input(f'Which attack(number)?\nATTACK\t\tDAMAGE\tCOST\npunch\t\t{player_attacks['punch']}\t{player_attack_cost[0]}\nkick\t\t{player_attacks['kick']}\t{player_attack_cost[1]}\nverbal abuse\t{player_attacks['verbal abuse']}\t{player_attack_cost[2]}\nfireball\t{player_attacks['fireball']}\t{player_attack_cost[3]}\npaper ball\t{player_attacks['Paperball']}\t{player_attack_cost[4]}\n'))
-            if player_points > player_attack_cost[player_attack-1]-1 and player_attack > 0 and player_attack < 6:
-                player_points -= player_attack_cost[player_attack-1]
-                bob_health -= (player_attacks[player_attack_names[player_attack-1]] - bob_defense)
-                print(f'Points Left: {player_points}')
-                print(f'you used {player_attack_names[player_attack-1]}')
-                input(f'bob took {player_attacks[player_attack_names[player_attack-1]] - bob_defense} damage, {bob_health} reamining\n')
-                current_paction = 'none'
-            elif player_attack > 0 and player_attack < 6:
-                input(f'not enough points dingus\n')
+            player_attack = input(f'Which attack(number)?\nATTACK\t\tDAMAGE\tCOST\npunch\t\t{player_attacks['punch']}\t{player_attack_cost[0]}\nkick\t\t{player_attacks['kick']}\t{player_attack_cost[1]}\nverbal abuse\t{player_attacks['verbal abuse']}\t{player_attack_cost[2]}\nfireball\t{player_attacks['fireball']}\t{player_attack_cost[3]}\npaper ball\t{player_attacks['Paperball']}\t{player_attack_cost[4]}\n')
+            if player_attack.isnumeric():
+                player_attack = int(player_attack)
+                if player_attack > 0 and player_attack < 6 and player_points > player_attack_cost[player_attack-1]-1:
+                    player_points -= player_attack_cost[player_attack-1]
+                    bob_health -= (player_attacks[player_attack_names[player_attack-1]] - bob_defense)
+                    print(f'Points Left: {player_points}')
+                    print(f'you used {player_attack_names[player_attack-1]}')
+                    input(f'bob took {player_attacks[player_attack_names[player_attack-1]] - bob_defense} damage, {bob_health} reamining\n')
+                    current_paction = 'none'
+                elif player_attack > 0 and player_attack < 6:
+                    input(f'not enough points dingus\n')
+                else:
+                    input(f'try again big dawg')
             else:
                 input(f'try again bub\n')
     if current_paction == 'action':
